998726b2e3a64a82e379817b1e677b16df9bef71
SauloCouto <saulocouto1221@gmail.com>
2024-02-02 09:43:16 -0300

    buffered writer add in class writer service

--- src/main/java/com/course/workfiles/exercise/service/WriterService.java
+++ src/main/java/com/course/workfiles/exercise/service/WriterService.java
@@ -2,17 +2,18 @@ package com.course.workfiles.exercise.service;
 
 import com.course.workfiles.exercise.entities.Relatory;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class WriterService {
 
-    public void writeFile(String fileName, List<Relatory> relatories) {
-        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
+    public void writeFile(String fileName,  List<Relatory> relatories) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true))) {
             relatories.forEach( relatory -> {
                 try {
-                    fileWriter.write(String.valueOf(relatory));
+                    bufferedWriter.write(String.valueOf(relatory));
                 }
                 catch (IOException e) {
                     System.out.println("Error: " + e.getMessage());
